c2857c2ae8c6c5af83a8e1c9145d2472099680bd
Will Baik <wbaik7022@stu.d214.org>
2025-04-11 11:06:15 -0700

    Added second textfield

--- ContentView.swift
+++ ContentView.swift
@@ -4,8 +4,10 @@ struct ContentView: View {
     @State var number1 = 0
     @State var number2 = 0
     var body: some View {
-        VStack {
-            TextField("Enter a Number", value: $number1, format: .number)
-        }
+      
+        TextField("Enter a Number", value: $number1, format: .number)
+        
+        TextField("Enter a Number", value: $number2, format: .number)
+        
     }
 }
