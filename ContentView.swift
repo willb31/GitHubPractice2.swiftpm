97d0e3cd1f487e280f431a3d5f2b58e2469e4b10
Dylan Widmayer <dwidmayer8476@stu.d214.org>
2025-04-11 13:11:16 -0500

    button added

--- ContentView.swift
+++ ContentView.swift
@@ -3,11 +3,17 @@ import SwiftUI
 struct ContentView: View {
     @State var number1 = 0
     @State var number2 = 0
+    @State var answer  = 0
     var body: some View {
       
         TextField("Enter a Number", value: $number1, format: .number)
         
         TextField("Enter a Number", value: $number2, format: .number)
+        Button("calulate") {
+            answer = number1 + number2
+            
+        }
+        Text("\(answer)")
         
     }
 }
